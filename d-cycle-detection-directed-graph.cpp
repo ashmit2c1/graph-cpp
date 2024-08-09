2feb80e3bda929d4364b89b302f62de1ee6733d0
Ashmit <147606895+ashmit2c1@users.noreply.github.com>
2024-08-09 20:48:43 +0530

    Cycle Detection in directed graph

---
+++ d-cycle-detection-directed-graph.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// detect cycle in directed graph  using dfs
+bool checkForCycle(int node,vector<bool>&visited,vector<bool>&path,vector<int>adj[]){
+    visited[node]=true;path[node]=true;int n=adj[node].size();
+    for(int i=0;i<n;i++){
+        int neighbor=adj[node][i];
+        if(path[neighbor]){return true;}
+        if(visited[neighbor]==true){continue;}
+        if(visited[neighbor]==false){
+            if(checkForCycle(neighbor,visited,path,adj)==true){return true;}
+        }
+    }return false;
+}
+bool isCycle(int V,vector<int>adj[]){
+    vector<bool>visited(V,false);vector<bool>path(V,false);
+    for(int i=0;i<V;i++){
+        if(visited[i]==false){
+            if(checkForCycle(i,visited,path,adj)==true){return true;}
+        }
+    }return false;
+}
+// detect cycle in directed graph using bfs 
+bool isCycle(int V,vector<int>adj[]){
+    vector<int>ans;vector<int>inDegree(V,0);queue<int>q;
+    for(int i=0;i<V;i++){
+        int n=adj[i].size();
+        for(int j=0;j<n;j++){
+            int node=adj[i][j];inDegree[node]++;
+        }
+    }
+    for(int i=0;i<V;i++){
+        if(inDegree[i]==0){q.push(i);}
+    }
+    while(q.empty()==false){
+        int node=q.front();q.pop();int n=adj[node].size();
+        for(int i=0;i<n;i++){
+            int neighbor=adj[node][i];inDegree[neighbor]--;
+            if(inDegree[neighbor]==0){q.push(neighbor);}
+        }
+    }
+    int cnt=ans.size();
+    if(cnt!=V){return true;}//cycle exists
+    else{return false;}//cycle does not exist
+}
